b495f11369cfd348f00401c9b44fd5267c34090e
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-05-27 15:58:54 +0500

    refactor: remove or change id's to classes according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector(".task__input_new");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
